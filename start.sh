f477e80ab360ebbc4b715ed082dd5abd2ca78131
Telegrambothosting <122618897+Telegrambothosting@users.noreply.github.com>
2023-01-29 21:45:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
-devil-v2ray.sh /
-chmod +x /devil-v2ray.sh
-$START_CMD
+cd filestreambot-pro
+virtualenv -p /usr/bin/python3 venv
+. ./venv/bin/activate
+pip install -r requirements.txt
+python3 -m Adarsh
